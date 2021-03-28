c4624017fe10840970ee57b56c1c5a5d691ddf1b
remerdy1 <62181722+remerdy1@users.noreply.github.com>
2021-03-28 02:28:23 +0100

    Improve console 1

--- Assets/Scripts/Console/ConsoleScript.cs
+++ Assets/Scripts/Console/ConsoleScript.cs
@@ -6,19 +6,24 @@ using UnityEngine.UI;
 public class ConsoleScript : MonoBehaviour
 {
     public Text spawnText;
-    public GameObject robot;
+    public GameObject soloBot;
+    public GameObject room1;
     public Animator robotAnim;
+    int activeBots;
+    public int maxBots;
+
     // Start is called before the first frame update
     void Start()
     {
         spawnText.enabled = false;
-        robot.SetActive(false);
+        activeBots = 0;
+        maxBots = 3;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        spawnText.text = $"Press F to spawn bot ({activeBots}/{maxBots})";
     }
 
     private void OnTriggerEnter(Collider other) {
@@ -26,11 +31,14 @@ public class ConsoleScript : MonoBehaviour
     }
 
     private void OnTriggerStay(Collider other) {
-        if(Input.GetKeyDown(KeyCode.F)){
+        if(Input.GetKeyDown(KeyCode.F) && activeBots < maxBots){
             // Spawn bot
-            robot.SetActive(true);
+            GameObject newBot = Instantiate(soloBot, new Vector3(36.92751f, 0.1000004f, -25.97998f), soloBot.transform.rotation);
+            newBot.transform.parent = room1.transform;
+            activeBots++;
+
             // Play animation
-            robotAnim.SetTrigger("fPressed?");
+            newBot.GetComponent<Animator>().SetTrigger("fPressed?");
         }
     }
     private void OnTriggerExit(Collider other) {
