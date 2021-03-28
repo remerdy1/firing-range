d1a7b58de21d3d997ccecf6e54e3f24a7c780b7f
remerdy1 <62181722+remerdy1@users.noreply.github.com>
2021-03-28 03:37:42 +0100

    Add paused

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,10 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     public float health = 100f;
 
     public bool isHoldingWeapon = false;
+    public Text pausedText;
+    bool paused;
+
+    private void Start() {
+        paused = false;
+        pausedText.enabled = false;
+    }
+
+    private void Update() {
+        // Paused 
+        if(Input.GetKeyDown(KeyCode.Escape) && !paused){
+            paused = true;
+            Time.timeScale = 0;
+            pausedText.enabled = true;
+        }else if(Input.GetKeyDown(KeyCode.Escape) && paused){
+            paused = false;
+            Time.timeScale = 1;
+            pausedText.enabled = false;
+        }
+    }
 }
