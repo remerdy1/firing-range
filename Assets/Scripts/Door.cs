9eb5efb131a1def2a264d6b90c77b76613e7bee6
remerdy1 <62181722+remerdy1@users.noreply.github.com>
2021-03-31 13:15:56 +0100

    Add generator room and 3d text

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        Debug.Log("Lit");
+        anim.SetTrigger("character_nearby");
+    }
+
+    private void OnTriggerExit(Collider other) {
+        anim.ResetTrigger("character_nearby");
+    }
+}
